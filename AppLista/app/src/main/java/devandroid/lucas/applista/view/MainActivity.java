a37cb13bfb328b333cdcd5f60426559426e05029
Lucas Fernando <107879275+fernandoppm@users.noreply.github.com>
2023-05-05 16:46:09 -0300

    Atualização do layout, e uso dos métodos Get e Set

--- AppLista/app/src/main/java/devandroid/lucas/applista/view/MainActivity.java
+++ AppLista/app/src/main/java/devandroid/lucas/applista/view/MainActivity.java
@@ -10,11 +10,48 @@ import devandroid.lucas.applista.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
+    Pessoa outraPessoa;
+
+    String dadosPessoa;
+    String dadosOutraPessoa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
         pessoa = new Pessoa();
+        pessoa.setPrimeiroNome("Lucas");
+        pessoa.setSobrenome("Fernando");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("81-940028922");
+
+
+        outraPessoa = new Pessoa();
+        outraPessoa.setPrimeiroNome("Maria");
+        outraPessoa.setSobrenome("Fatima");
+        outraPessoa.setCursoDesejado("Python");
+        outraPessoa.setTelefoneContato("81-77567788");
+
+        dadosPessoa = "Primeiro nome: ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa += " Sobrenome: ";
+        dadosPessoa += pessoa.getSobrenome();
+        dadosPessoa += " Curso Desejado: ";
+        dadosPessoa += pessoa.getCursoDesejado();
+        dadosPessoa += " Telefone de Contato: ";
+        dadosPessoa += pessoa.getTelefoneContato();
+
+        dadosOutraPessoa = "Primeiro nome: ";
+        dadosOutraPessoa += outraPessoa.getPrimeiroNome();
+        dadosOutraPessoa += " Sobrenome: ";
+        dadosOutraPessoa += outraPessoa.getSobrenome();
+        dadosOutraPessoa += " Curso Desejado: ";
+        dadosOutraPessoa += outraPessoa.getCursoDesejado();
+        dadosOutraPessoa += " Telefone de Contato: ";
+        dadosOutraPessoa += outraPessoa.getTelefoneContato();
+
+
+
     }
 }
